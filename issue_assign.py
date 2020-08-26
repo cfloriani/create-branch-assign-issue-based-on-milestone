62266ce626f6c9d88cf4a98e07d2f3c22512855f
Floriani <carlos.floriani@sci10.com.br>
2020-08-26 17:35:18 -0300

    erros

--- issue_assign.py
+++ issue_assign.py
@@ -9,12 +9,10 @@ repo = g.get_repo(os.getenv('repository'))
 # cria uma branch para a issue
 branch_issue = os.getenv('issue_num') + '-' + slugify(os.getenv('issue'))
 repo.create_git_ref('refs/heads/{branch_issue}'.format(**locals()),repo.get_branch('master').commit.sha)
-print('Criada a branch ', branch_issue ,' para a Issue')
 
 # cria uma branch extra para a issue com milestone feature:
 if str(os.getenv('milestone'))[0:7].lower() == "feature":
     branch_milestone = slugify(os.getenv('milestone'))
-    print('Criada a branch ', branch_milestone ,' para a Milestone')
     repo.create_git_ref('refs/heads/{branch_milestone}'.format(**locals()),repo.get_branch('master').commit.sha)
     repo.create_pull(title=branch_issue, body='#' + os.getenv('issue_num'), head=os.getenv('user') + ':' + branch_milestone, base=branch_milestone)
 else:
