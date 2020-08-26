bbb9612ba367e6aa47871f20c719b464a187c9a4
Carlos Eduardo <60077621+cfloriani@users.noreply.github.com>
2020-08-26 16:12:20 -0300

    Update issue_assign.py

--- issue_assign.py
+++ issue_assign.py
@@ -9,10 +9,10 @@ repo = g.get_repo(os.getenv('repository'))
 # cria uma branch para a issue
 nome_branch = slugify(os.getenv('issue'))
 repo.create_git_ref('refs/heads/{nome_branch}'.format(**locals()),repo.get_branch('master').commit.sha)
-print('Criada a branch ', nome_branch ,' para a Issue)
+print('Criada a branch ', nome_branch ,' para a Issue')
 
 # cria uma branch extra para a issue com milestone feature:
 if str(os.getenv('milestone'))[0:7].lower() == "feature":
     nome_branch = slugify(os.getenv('milestone'))
-    print('Criada a branch ', nome_branch ,' para a Milestone)
+    print('Criada a branch ', nome_branch ,' para a Milestone')
     repo.create_git_ref('refs/heads/{nome_branch}'.format(**locals()),repo.get_branch('master').commit.sha)
